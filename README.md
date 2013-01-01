52af16cbd76612e015b6f63fa212b1584a2b14ba
James Jolly <jamesjolly@gmail.com>
2012-12-31 21:38:15 -0600

    class names are now dirs, example names filenames, and PR now computed correctly

--- README.md
+++ README.md
@@ -7,13 +7,26 @@ The validation pipeline is:
 input_img_dir --> process_dir.py --> feature_vector_file --> bench_knn.py
 
 input_img_dir should contain subdirs that each represent an image class,
-containing examples in .png format.  Initial testing has been against
-a subset of the Amsterdam Library of Object Images.
+containing examples in .png format.  For example...
+
+input_img_dir/class1/example1.png
+input_img_dir/class1/example2.png
+input_img_dir/class1/example3.png
+input_img_dir/class2/example1.png
+input_img_dir/class2/example2.png
+
+... could be labels extracted from a input_img_dir. In other words, each 
+label from input_img_dir contains both a class name (like 'class1') and 
+an example name ('like example3.png').
+
+Initial testing has been against a subset of the Amsterdam Library of 
+Object Images.
 
 This uses KNN under the hood (see get_nearest_k in knn.py). It doesn't
-leverage an index yet so it will not scale to large datasets.
+leverage an index (yet) so it will not scale to large datasets.
 
 I'd like this to work against most image collections, so I'm using 
 the most primitive scale-invariant features imaginable: the means of
 the red, green, and blue components of the image pixel values as well 
 as the standard deviation of the grayscale version of each pixel.
+
--- bench_knn.py
+++ bench_knn.py
@@ -9,12 +9,12 @@ from collections import defaultdict
 import knn
 
 c_benchmark_k_start = 3
-c_benchmark_k_end = 3
-c_class_size = 2
+c_benchmark_k_end = 6
 
 def get_class(filename):
    # ex. input: 'simpledata/421/421_c.png'
-   return filename.split('_')[0]
+   # (class is '421')
+   return filename.split('/')[-2:][0]
 
 def get_feature_ranges(labeled_vectors):
    # needed for normalization
@@ -26,6 +26,13 @@ def get_feature_ranges(labeled_vectors):
        feature_maxes.append(max(values))
    return feature_mins, feature_maxes
 
+def get_class_counts(labeled_vectors):
+    # returns dict of class name to instance count
+    class_counts = defaultdict(int)
+    for label, vector in labeled_vectors:
+        class_counts[get_class(label)] += 1
+    return class_counts
+
 def load_labeled_vectors(infile):
    labeled_vectors = [ ]
    for line in infile:
@@ -36,11 +43,13 @@ def load_labeled_vectors(infile):
    return labeled_vectors
 
 def normalize_vectors(feature_vectors, feat_mins, feat_maxes):
-   normalized_vectors = [ ]
-   for label, vector in feature_vectors:
-      normalized_vector = knn.normalize(vector, feat_mins, feat_maxes)
-      normalized_vectors.append((label, normalized_vector))
-   return normalized_vectors
+    # scales each vector in feature_vectors to [0, 1] based upon ranges
+    # established by feat_mins, feat_maxes
+    normalized_vectors = [ ]
+    for label, vector in feature_vectors:
+       normalized_vector = knn.normalize(vector, feat_mins, feat_maxes)
+       normalized_vectors.append((label, normalized_vector))
+    return normalized_vectors
 
 if __name__ == "__main__":
 
@@ -52,22 +61,27 @@ if __name__ == "__main__":
    feature_vectors = load_labeled_vectors(infile)
    feature_mins, feature_maxes = get_feature_ranges(feature_vectors)
    norm_vects = normalize_vectors(feature_vectors, feature_mins, feature_maxes)
+   class_counts = get_class_counts(feature_vectors)
+   num_vectors = float(len(feature_vectors))
 
+   print "K\tPRECISION\tRECALL\n- - - - - - - - - - - - - - - -"
+
+   # issue search for each vector (see get_nearest_k)
+   # compute average precision and recall over all vectors for each k
    for k in range(c_benchmark_k_start, c_benchmark_k_end + 1):
-      precision_sum = 0.0
-      recall_sum = 0.0
+      precision_sum, recall_sum = 0.0, 0.0
       for query_label, query_vector in feature_vectors:
          normalized_query_vector = knn.normalize(query_vector, feature_mins, feature_maxes)
          closest = knn.get_nearest_k(normalized_query_vector, norm_vects, k)
 
-         label_scores = defaultdict(float)
-         for distance, labeled_vector in closest[1:]:
+         correct = 1.0
+         class_scores = defaultdict(float)
+         for distance, labeled_vector in closest[1:]: # don't consider closest vector, itself
              label, vector = labeled_vector
-             label_scores[get_class(label)] += 1.0/(1.0 + distance)
+             if get_class(label) == get_class(query_label):
+                 correct += 1.0
+         precision_sum += correct/float(k)
+         recall_sum += correct/class_counts[get_class(query_label)]
 
-         guess_label, guess_vector = sorted(label_scores.items(), key=lambda item: item[1]).pop()
-         precision_sum += float(guess_label == get_class(query_label))
-         num_vectors = float(len(feature_vectors))
-
-      print k, precision_sum/num_vectors
+      print "%d\t%f\t%f" % (k, precision_sum/num_vectors, recall_sum/num_vectors)
 
